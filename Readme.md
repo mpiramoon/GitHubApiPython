3cf0c8705bbeb3759d202402fd2a10ff35d1ddfb
mpiramoon <m.piramoon@gmail.com>
2022-07-09 21:45:30 -0400

    PullandRelease

---
+++ Readme.md
@@ -0,0 +1,45 @@
+# Preparation
+
+First, you need to install **requests** on your python
+
+## Windows
+
+open command prompt from the windows and run following command:
+
+```
+ python -m pip install requests
+```
+
+## Linux
+
+Run:
+
+```
+ pip install requests
+```
+
+## macOS
+
+Run:
+
+```
+ pip install requests
+```
+
+# Using Program:
+
+For using this code, you need to know a name of the owner and a repository name for using this program. after providing these names, Run:
+
+```
+ python mp.py dbt-labs dbt-core
+```
+
+## Private repository:
+
+If you want to check the private repository, you need to provide a token. [Creating a personal access token](https://docs.github.com/en/enterprise-server@3.4/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)
+
+After that you can access the private repository by running:
+
+```
+python mp.py mpiramoon Trip -t=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
+```
---
+++ mp.py
@@ -0,0 +1,85 @@
+
+import argparse
+import requests
+
+
+def getpullrequests(owner, repo, token):
+    headers={}
+    if(token!=""):
+        headers = {'Authorization': f'token {token}'}
+
+    query_url = f"https://api.github.com/repos/{owner}/{repo}/pulls"
+    params = {
+        "state": "open",
+    }
+
+    try:
+        r = requests.get(query_url, headers=headers, params=params)
+        result =r.json()
+        #Show error message if there is any message
+        if 'message' in result:
+            print(f"{result['message']}")
+            raise Exception()
+    
+        end=0
+        #check if there are more than three result
+        if(len(result)>3):
+            end=3
+        else:
+            end=len(result)
+        
+        print(f"\n {end} most recent pull requests: \n")
+        
+        for rep in result[0:end]:
+             res=f"title:'{rep['title']}', number:{rep['number']}"
+             print(res)
+
+    except :
+         print("please check your parameters")
+
+
+
+def getreleases(owner, repo, token):
+    headers={}
+    if(token!=""):
+        headers = {'Authorization': f'token {token}'}
+
+    query_url = f"https://api.github.com/repos/{owner}/{repo}/releases"
+    params = {
+        "state": "open",
+    }
+
+    try:
+        r = requests.get(query_url, headers=headers, params=params)
+        result =r.json()
+        #Show error message if there is any message
+        if 'message' in result:
+            print(f"{result['message']}")
+            raise Exception()
+    
+        end=0
+        #check if there are more than three result
+        if(len(result)>3):
+            end=3
+        else:
+            end=len(result)
+        
+        print(f"\n {end} most recent releases: \n")
+        
+        for rep in result[0:end]:
+             res=f"name:'{rep['name']}'"
+             print(res)
+
+    except :
+         print("please check your parameters")
+
+
+if __name__=='__main__':
+   parser = argparse.ArgumentParser(description='Showing three latest releases and three most recent pull requests')
+   parser.add_argument("owner", type=str,help="Specify the project owner")
+   parser.add_argument("repo", type=str,help="Specify the repository name")
+   parser.add_argument("--token","-t", type=str,help="Specify the Token if you want to access a private repository",default="",required=False)
+   args = parser.parse_args()
+   getpullrequests(args.owner,args.repo,args.token)
+   getreleases(args.owner,args.repo,args.token)
+
